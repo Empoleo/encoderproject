16b7beb4fb6058a51b9cd6bce4775a11e1ecc8fb
Charles Dowell <iggydowell@gmail.com>
2016-10-10 10:45:12 -0700

    echoes

---
+++ encoder.js
@@ -0,0 +1,6 @@
+$("#inputArea").keydown(function(e) {
+  if(e.keyCode == 13) {
+    $("#textArea").append(document.getElementById('inputText').value += " ");
+    document.getElementById('inputText').value = "";
+}
+});
